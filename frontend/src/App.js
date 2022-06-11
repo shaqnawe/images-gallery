62c8abaabbb22851598d3d38c2c21bf2ace5e925
shaqnawe <shaktishah40@gmail.com>
2022-06-10 22:20:34 -0500

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <div>
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery" />
     </div>
   );
-}
+};
 
 export default App;
